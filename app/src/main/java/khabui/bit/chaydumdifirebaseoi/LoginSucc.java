4cd1a0c6dd763386edac87ee1f7e3b9dc34e6c8c
KhaB-BIT <khabui.31191025985@st.ueh.edu.vn>
2022-03-16 23:08:47 +0700

    'abc'

---
+++ app/src/main/java/khabui/bit/chaydumdifirebaseoi/LoginSucc.java
@@ -0,0 +1,14 @@
+package khabui.bit.chaydumdifirebaseoi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginSucc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_succ);
+    }
+}
